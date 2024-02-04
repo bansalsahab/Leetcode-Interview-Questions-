0179ce135698801f346637983d01166d6d48e611
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-02-05 00:30:02 +0530

    Update Permutations.java

--- Google/medium/Permutations.java
+++ Google/medium/Permutations.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> subsets=new ArrayList<>() ;
+        generatPermute(nums,new ArrayList<>(),subsets);
+        return subsets;
+    }
+    void generatPermute(int[] nums,List<Integer> current,List<List<Integer>>subsets){
+    if(current.size()==nums.length){
+        subsets.add(new ArrayList<>(current));
+        return;
+    }
+    for(int i=0 ;i<nums.length; i++){
+        if(current.contains(nums[i]))
+        continue;
+        current.add(nums[i]);
+        generatPermute(nums,current,subsets);
+        current.remove(current.size()-1);
+    }}
+}
