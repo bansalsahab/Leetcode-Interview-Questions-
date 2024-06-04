860a8fe600f3e5a76397b36b1b3e881e5572fcb3
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2024-06-04 22:46:07 +0530

    Update Non-overlapping Intervals.java

--- Interval Questions/Non-overlapping Intervals.java
+++ Interval Questions/Non-overlapping Intervals.java
@@ -1,1 +1,26 @@
 
+class Solution {
+  public int eraseOverlapIntervals(int[][] intervals) {
+    if (intervals.length == 0) {
+      return 0;
+    }
+
+    // Sort intervals based on their ending times
+    Arrays.sort(intervals, (val1, val2) -> val1[1] - val2[1]);
+
+    int count = 1;
+    int lastEndTime = intervals[0][1];
+    int n = intervals.length;
+
+    for (int i = 1; i < n; i++) {
+      // Check if the current interval overlaps with the previous one
+      if (intervals[i][0] >= lastEndTime) {
+        // No overlap, update the end time
+        count++;
+        lastEndTime = intervals[i][1];
+      }
+    }
+
+    return n - count;
+  }
+}
