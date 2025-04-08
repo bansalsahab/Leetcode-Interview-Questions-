4dfd7a09e9bc3d7956508d7774b12becb6fcdab6
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2025-04-08 18:35:52 +0530

    Update Minimum Time to Collect All Apples in a Tree.java

--- GRAPHS/Minimum Time to Collect All Apples in a Tree.java
+++ GRAPHS/Minimum Time to Collect All Apples in a Tree.java
@@ -1,1 +1,28 @@
-
+class Solution {
+    public int dfs(int src ,int parent , List<List<Integer>> adjList,List<Boolean> hasApple){
+        int TotalTime =0 ;
+        for(int neighbour : adjList.get(src)){
+            if(neighbour == parent) continue;
+            int timetakenBychild = dfs(neighbour,src,adjList,hasApple);
+            if(timetakenBychild > 0 || hasApple.get(neighbour)){
+                TotalTime += timetakenBychild +2;
+            }
+        }
+        return TotalTime;
+    }
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        //adj list
+        List<List<Integer>> adjList =new ArrayList<>();
+        for(int i =0; i < n;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int edge[] :edges){
+            int from = edge[0];
+            int to = edge[1];
+            adjList.get(from).add(to);
+            adjList.get(to).add(from);
+        }
+        return dfs(0,-1,adjList,hasApple);
+    }
+    
+}
