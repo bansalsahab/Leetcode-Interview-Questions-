8f03147ed6691e4969db655c5859278593fd9f20
Parth bansal <120677173+bansalsahab@users.noreply.github.com>
2026-03-20 23:37:02 +0530

    Update Max Consecutive Ones III.java

--- Sliding Window/Max Consecutive Ones III.java
+++ Sliding Window/Max Consecutive Ones III.java
@@ -25,3 +25,33 @@ class Solution {
         return maxCount;
     }
 }
+
+//Longest Subarray of 1's After Deleting One Element
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int windowStart = 0;
+        int n = nums.length;
+        int windowEnd = 0;
+        int zeros = 0;
+        int maxCount = 0;
+
+        while (windowEnd < n) {
+            if (nums[windowEnd] == 0) {
+                zeros++;
+            }
+
+              while (zeros > 1) {
+                if (nums[windowStart] == 0) {
+                    zeros--;
+                }
+                windowStart++;
+            }
+
+            maxCount = Math.max(maxCount, windowEnd - windowStart );
+            windowEnd++;
+        }
+
+        return maxCount;
+    }
+}
+    
